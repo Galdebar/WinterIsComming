1a97c588538f91b08ecd48e578654b4d28f5ed4b
Galdebar <Galdebar@gmail.com>
2021-07-11 09:50:37 +0300

    init commit

---
+++ src/main/java/lt/galdebar/winteriscomming/WinteriscommingApplication.java
@@ -0,0 +1,13 @@
+package lt.galdebar.winteriscomming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinteriscommingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinteriscommingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/galdebar/winteriscomming/WinteriscommingApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.galdebar.winteriscomming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinteriscommingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
